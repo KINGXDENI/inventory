828e455f30fbe92ed7764dcb05a59d7d214573e5
KINGBOTs1 <ds767365@gmail.com>
2024-06-14 09:09:26 +0800

    Update template.php

--- app/Views/layouts/template.php
+++ app/Views/layouts/template.php
@@ -30,9 +30,9 @@
 </head>
 
 <body>
-    <!-- <div id="global-loader">
+    <div id="global-loader">
         <div class="whirly-loader"> </div>
-    </div> -->
+    </div>
 
     <div class="main-wrapper">
 
